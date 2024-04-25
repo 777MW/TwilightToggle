5ccecbe91c96fc88edfe4f985e9d2e2b6963e610
MᴀxWᴏʀᴋs <58561933+777MW@users.noreply.github.com>
2024-04-25 14:03:26 -0400

    Imported TwilightToggleTray Beta
    Imported C# Solution and project files for TwilightToggleTray Beta version.

---
+++ TwilightToggleTray/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace TwilightToggleTray
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            notifyIcon1 = new NotifyIcon(components);
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            settingsToolStripMenuItem = new ToolStripMenuItem();
+            exitTwilightToggleToolStripMenuItem = new ToolStripMenuItem();
+            button1 = new Button();
+            contextMenuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+            notifyIcon1.Text = "TwilightToggle";
+            notifyIcon1.Visible = true;
+            notifyIcon1.Click += notifyIcon1_Click;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { settingsToolStripMenuItem, exitTwilightToggleToolStripMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(205, 52);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            settingsToolStripMenuItem.Size = new Size(204, 24);
+            settingsToolStripMenuItem.Text = "Settings";
+            settingsToolStripMenuItem.Click += settingsToolStripMenuItem_Click;
+            // 
+            // exitTwilightToggleToolStripMenuItem
+            // 
+            exitTwilightToggleToolStripMenuItem.Name = "exitTwilightToggleToolStripMenuItem";
+            exitTwilightToggleToolStripMenuItem.Size = new Size(204, 24);
+            exitTwilightToggleToolStripMenuItem.Text = "Exit TwilightToggle";
+            exitTwilightToggleToolStripMenuItem.Click += exitTwilightToggleToolStripMenuItem_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(50, 351);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "Save";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(259, 450);
+            ControlBox = false;
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "TwilightToggle Settings";
+            Load += Form1_Load;
+            contextMenuStrip1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem settingsToolStripMenuItem;
+        private ToolStripMenuItem exitTwilightToggleToolStripMenuItem;
+        private Button button1;
+    }
+}
---
+++ TwilightToggleTray/Form1.cs
@@ -0,0 +1,47 @@
+﻿namespace TwilightToggleTray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Hide the form on load
+            this.Opacity = 0.0;
+
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            if (contextMenuStrip1.Visible)
+            {
+                contextMenuStrip1.Visible = false;
+            }
+            else
+            {
+                notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+                //contextMenuStrip1.Visible = true;
+                contextMenuStrip1.Show(MousePosition.X, MousePosition.Y);
+            }
+        }
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Opacity = 1.0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.ShowBalloonTip(100, "🌙☀️ TwilightToggle", "🌙 User settings have been saved.", notifyIcon1.BalloonTipIcon);
+            this.Opacity = 0.0;
+        }
+
+        private void exitTwilightToggleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
